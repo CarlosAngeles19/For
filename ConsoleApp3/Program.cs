b67e8d28fc344f02ceb54c1ee0c8318e0e3f5846
CarlosAngeles19 <carlosangeles.asc@gmail.com>
2023-09-20 21:08:45 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese la base: ");
+        string basetxt=Console.ReadLine();
+        float base1=float.Parse(basetxt);
+
+        Console.Write("Ingresa el exponente: ");
+        string potenciatxt =Console.ReadLine();
+        int potencia = int.Parse(potenciatxt);
+
+        double resultado = 1;
+
+        for (int i = 0; i < potencia; i++)
+        {
+            resultado *= base1;
+        }
+
+        Console.WriteLine("La potencia es:" + resultado);
+        Console.ReadKey();
+    }
+}
